d858353f6d547b6913cfbf480ae59d7a52677e33
John Cronin <jncronin@tysos.org>
2019-02-02 14:47:01 +0000

    Automatically save to disk Add random value generator if no device in debug mode for testing purposes

--- temphumid-win/temphumid-gui/Form1.cs
+++ temphumid-win/temphumid-gui/Form1.cs
@@ -13,6 +13,8 @@ namespace temphumid_gui
     public partial class Form1 : Form
     {
         libtemphumidnet.Device d;
+        Random r;
+        System.IO.StreamWriter sw;
         
         public Form1()
         {
@@ -26,19 +28,29 @@ namespace temphumid_gui
         {
             if(d == null)
             {
+#if !RAND
                 label1.Text = "No Device";
                 label2.Text = "";
+#endif
 
                 if (chk_count-- == 0)
                 {
                     get_device();
                     chk_count = chk_interval;
                 }
+
+#if !RAND
             }
             else
             {
                 var temp = d.Temperature;
                 var humid = d.Humidity;
+#else
+                if (r == null)
+                    r = new Random();
+                var temp = r.NextDouble() * 100.0;
+                var humid = r.NextDouble() * 50.0;
+#endif
 
                 if (double.IsNaN(temp) || double.IsNaN(humid))
                 {
@@ -46,8 +58,21 @@ namespace temphumid_gui
                 }
                 else
                 {
-                    label1.Text = d.Temperature.ToString("F2") + " °C";
-                    label2.Text = d.Humidity.ToString("F2") + " %";
+#if RAND
+                    label1.Text = temp.ToString("F2") + " °C (R)";
+                    label2.Text = humid.ToString("F2") + " % (R)";
+#else
+                    label1.Text = temp.ToString("F2") + " °C";
+                    label2.Text = humid.ToString("F2") + " %";
+#endif
+                    if (sw != null)
+                    {
+                        var dt = System.DateTime.Now;
+                        sw.WriteLine(dt.ToString("yyyyMMdd\tHH:mm:ss.fff") + "\t" +
+                            temp.ToString("F2") + "\t" +
+                            humid.ToString("F2"));
+                        sw.Flush();
+                    }
                 }
             }
         }
@@ -65,6 +90,19 @@ namespace temphumid_gui
         private void Form1_Load(object sender, EventArgs e)
         {
             get_device();
+
+            var fname = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) +
+                "/temphumid-" +
+                System.DateTime.Now.ToString("yyyyMMdd") + ".txt";
+            try
+            {
+                var s = new System.IO.FileStream(fname, System.IO.FileMode.Append,
+                    System.IO.FileAccess.Write, System.IO.FileShare.ReadWrite);
+                sw = new System.IO.StreamWriter(s);
+
+                this.Text = this.Text + " " + fname;
+            }
+            catch (Exception) { }
 
             update();
 
